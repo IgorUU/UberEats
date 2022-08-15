9832d75354dde0c4f8008e2425511d47a64887c7
Igor <igor@studiopresent.com>
2022-08-15 15:16:30 +0200

    Add style to buttons

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -4,7 +4,9 @@ import { TouchableOpacity } from "react-native";
 
 const HeaderTabs = () => {
   return (
-    <View>
+    <View style={{
+      flexDirection: "row", alignSelf: "center"
+    }}>
       <HeaderButton text="Delivery" />
       <HeaderButton text="Pickup" />
       {/* HeaderButton */}
@@ -14,12 +16,16 @@ const HeaderTabs = () => {
 
 const HeaderButton = (props) => {
   return (
-    <View>
-      <TouchableOpacity>
-        <Text>{props.text}</Text>
+      <TouchableOpacity style={{
+        backgroundColor: "black",
+        paddingVertical: 6,
+        paddingHorizontal: 16,
+        borderRadius: 30
+      }}>
+        <Text style={{
+          color: "white", fontSize: 15, fontWeight: '900'
+        }}>{props.text}</Text>
       </TouchableOpacity>
-    </View>
-
   )
 }
 
