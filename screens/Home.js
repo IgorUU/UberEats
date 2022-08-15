f1bc4b9ef5bd24c63fab0e3298bd179e73de3bbd
Igor <igor@studiopresent.com>
2022-08-15 16:28:50 +0200

    Search bar in progress

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { View, Text, SafeAreaView } from 'react-native';
 import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 import SafeViewAndroid from "../styles/SafeViewAndroid";
 
 const Home = () => {
   return (
     <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-      <HeaderTabs />
+      <View style={{backgroundColor: 'white', padding: 15}}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
     </SafeAreaView>
   )
 }
