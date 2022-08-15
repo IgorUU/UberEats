174006b0b53019972ff8acd6fff55395e8413e78
Igor <igor@studiopresent.com>
2022-08-15 23:27:01 +0200

    Finished the search bar and started the Categories component

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text } from 'react-native';
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";;
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const SearchBar = () => {
   return (
@@ -22,6 +24,24 @@ const SearchBar = () => {
             marginRight: 10
           }
         }}
+        renderLeftButton = {() => (
+          <View style={{marginLeft: 10}}>
+            <Ionicons name="location-sharp" size={24} />
+          </View>
+        )}
+        renderRightButton = {() => (
+          <View style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginRight: 8,
+            backgroundColor: "white",
+            padding: 9,
+            borderRadius: 30
+          }}>
+            <AntDesign name='clockcircle' size={11} style={{marginRight: 6}} />
+            <Text>Search</Text>
+          </View>
+        )}
       />
     </View>
   )
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, SafeAreaView } from 'react-native';
+import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
 import SafeViewAndroid from "../styles/SafeViewAndroid";
@@ -11,6 +12,7 @@ const Home = () => {
         <HeaderTabs />
         <SearchBar />
       </View>
+      <Categories />
     </SafeAreaView>
   )
 }
