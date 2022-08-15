d32ec61839692d2d462ced7de908a4ca24ac6ce3
Igor <igor@studiopresent.com>
2022-08-15 23:52:30 +0200

    Working on categories

--- components/Categories.js
+++ components/Categories.js
@@ -1,10 +1,33 @@
 import React from "react";
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const items = [
+  {
+    image: require("../assets/images/shopping-bag.png"),
+    text: 'Pick-up'
+  },
+  {
+    image: require("../assets/images/soft-drink.png"),
+    text: 'Soft Drinks'
+  },
+  {
+    image: require("../assets/images/bread.png"),
+    text: 'Bakery Items'
+  },
+  {
+    image: require("../assets/images/fast-food.png"),
+    text: 'Fast Food'
+  }
+];
 
 const Categories = () => {
   return (
     <View>
-      <Text>Categories</Text>
+      <Image source={items[0].image} style={{
+        width: 50,
+        height: 40,
+        resizeMode: "contain",
+      }}/>
     </View>
   )
 }
