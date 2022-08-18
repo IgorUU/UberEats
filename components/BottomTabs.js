01dc3e1ca5dc0f3ce0c32747e0a126e5c7925c3e
Igor <igor@studiopresent.com>
2022-08-18 23:34:32 +0200

    Refactor and finish bottom tabs component

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -1,60 +1,48 @@
 import React from "react";
 import { View, Text } from "react-native";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 const BottomTabs = () => {
+  const iconsData = [
+    ["home", "Home"],
+    ["search", "Browse"],
+    ["shopping-bag", "Grocery"],
+    ["list", "Orders"],
+    ["user", "Account"],
+  ];
+
   return (
     <View
       style={{
-        padding: 5,
+        margin: 10,
+        marginHorizontal: 30,
         flexDirection: "row",
         justifyContent: "space-between",
       }}
     >
-      <View
-        style={{
-          alignSelf: "center",
-          alignContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <FontAwesome name="home" size={30} />
-        <Text>Home</Text>
-      </View>
-      <View
-        style={{
-          alignItems: "center",
-        }}
-      >
-        <FontAwesome name="search" size={30} />
-        <Text>Browse</Text>
-      </View>
-      <View
-        style={{
-          alignItems: "center",
-        }}
-      >
-        <FontAwesome name="shopping-bag" size={30} />
-        <Text>Grocery</Text>
-      </View>
-      <View
-        style={{
-          alignItems: "center",
-        }}
-      >
-        <FontAwesome name="list" size={30} />
-        <Text>Orders</Text>
-      </View>
-      <View
-        style={{
-          alignItems: "center",
-        }}
-      >
-        <FontAwesome name="user-o" size={30} />
-        <Text>Account</Text>
-      </View>
+      {iconsData.map((ikona) => (
+        <Icon icon={ikona[0]} text={ikona[1]} />
+      ))}
     </View>
   );
 };
+
+const Icon = (props) => (
+  <View
+    style={{
+      alignItems: "center",
+    }}
+  >
+    <FontAwesome5
+      name={props.icon}
+      size={25}
+      style={{
+        marginBottom: 3,
+        alignSelf: "center",
+      }}
+    />
+    <Text>{props.text}</Text>
+  </View>
+);
 
 export default BottomTabs;
