fc23a5cfd5f473ca93b3fe150ebfc3d956e3970b
Igor <igor@studiopresent.com>
2022-08-19 16:04:41 +0200

    Finished hard-coded version of About component

--- components/restaurantDetail/About.js
+++ components/restaurantDetail/About.js
@@ -1,12 +1,58 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
+
+const image =
+  "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2022%2F03%2F29%2Fpasta-power-tout.jpg";
+
+const title = "Farmhouse Kitchen Thai Cuisine";
+const description = "Thai · Comfort food · $$ · 🎫 · 4 ⭐ (2912+)";
 
 const About = () => {
   return (
     <View>
-      <Text>About</Text>
+      <RestaurantImage image_url={image} />
+      <RestaurantTitle title={title} />
+      <RestaurantDescription description={description} />
     </View>
   );
 };
+
+const RestaurantImage = (props) => (
+  <Image
+    source={{
+      uri: props.image_url,
+    }}
+    style={{
+      width: "100%",
+      height: 180,
+    }}
+  />
+);
+
+const RestaurantTitle = (props) => (
+  <Text
+    style={{
+      fontWeight: "bold",
+      fontSize: 29,
+      marginTop: 10,
+      marginHorizontal: 15,
+    }}
+  >
+    {props.title}
+  </Text>
+);
+
+const RestaurantDescription = (props) => (
+  <Text
+    style={{
+      marginTop: 10,
+      marginHorizontal: 15,
+      fontWeight: "400",
+      fontSize: 15.5,
+    }}
+  >
+    {props.description}
+  </Text>
+);
 
 export default About;
--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { Divider } from "react-native-elements";
+import About from "../components/restaurantDetail/About";
 
 const RestaurantDetails = () => {
   return (
     <View>
-      <Text>Restaurant Details</Text>
+      <About />
+      <Divider width={1.8} style={{ marginVertical: 20 }} />
     </View>
   );
 };
