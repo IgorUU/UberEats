396be5d24224e083298cad7f4f2ce2ca2c4311d7
Igor <igor@studiopresent.com>
2022-08-23 15:00:30 +0200

    Adding Menu Items component

--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Text, View } from "react-native";
 import { Divider } from "react-native-elements";
 import About from "../components/restaurantDetail/About";
+import MenuItems from "../components/restaurantDetail/MenuItems";
 
 const RestaurantDetails = () => {
   return (
     <View>
       <About />
       <Divider width={1.8} style={{ marginVertical: 20 }} />
+      <MenuItems />
     </View>
   );
 };
