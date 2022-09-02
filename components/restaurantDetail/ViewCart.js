b510b8dabebdaab3548f22fe2c47d542d78cca84
Igor <igor@studiopresent.com>
2022-09-02 19:46:26 +0200

    Create ViewCart compoment (button)

---
+++ components/restaurantDetail/ViewCart.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+
+const ViewCart = (navigation, ...props) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        flexDirection: "row",
+        position: "absolute",
+        bottom: 30,
+        zIndex: 999,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            position: "relative",
+            marginTop: 20,
+            padding: 13,
+            borderRadius: 30,
+            alignItems: "center",
+            width: 300,
+            backgroundColor: "black",
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 20 }}>View Cart</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default ViewCart;
--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -3,8 +3,9 @@ import { Text, View } from "react-native";
 import { Divider } from "react-native-elements";
 import About from "../components/restaurantDetail/About";
 import MenuItems from "../components/restaurantDetail/MenuItems";
+import ViewCart from "../components/restaurantDetail/ViewCart";
 
-const RestaurantDetails = ({ route }) => {
+const RestaurantDetails = ({ route, navigation }) => {
   return (
     <View
       style={{
@@ -14,6 +15,7 @@ const RestaurantDetails = ({ route }) => {
       <About route={route} />
       <Divider width={1.8} style={{ marginVertical: 20 }} />
       <MenuItems />
+      <ViewCart navigation={navigation} restaurantName={route.params.name} />
     </View>
   );
 };
